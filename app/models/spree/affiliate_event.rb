7d7375ae227ab7d6fce188f3bf0c97e5d0cd6941
Chris Lindensmith <lindensmith@github>
2012-06-30 12:21:24 -0700

    updated create_affiliate_credits and affiliate_event model to prevent mass assign errors (probably due to Rails 3.2.3 changes)

--- app/models/spree/affiliate_event.rb
+++ app/models/spree/affiliate_event.rb
@@ -1,4 +1,7 @@
 class Spree::AffiliateEvent < ActiveRecord::Base
+  
+  attr_accessible :reward, :name, :user
+  
   belongs_to :affiliate, :class_name => 'Spree::Affiliate'
   belongs_to :reward, :polymorphic => true
   belongs_to :user, :class_name => 'Spree::User'
